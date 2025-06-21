b61cd1726ec5b32d06044e5ac902e999f3b73b51
PriyanshiPal18 <priyakusum2000@gmail.com>
2025-06-21 10:55:44 +0530

    Added Solution for Two Sum Problem - Easy C++

---
+++ TwoSum-Easy.cpp
@@ -0,0 +1,34 @@
+#include<algorithm>
+#include <iterator>
+#include<vector>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size()-1;
+        int sum,flag1=0,flag2=0;
+        vector<int> n2=nums;
+        sort(nums.begin(), nums.end());
+        
+        while(start<end){
+        sum=nums[start]+nums[end];
+        if(sum==target)
+           break;
+        else if (sum>target)
+            end--;
+        else if (sum<target)
+            start++;
+        } 
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[start]==n2[i] && flag1==0)
+                {start=i;flag1=1;}
+            else if(nums[end]==n2[i]&& flag2 ==0)
+                {end=i;flag2=1;}
+        }
+        return {start,end};
+    }
+   
+};
